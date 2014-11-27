fa2c4fa5670f78195516f1fa338ef0338f256784
Q <quintinsmith@icloud.com>
2014-11-27 11:45:25 -0700

    Initial Commit

---
+++ Very Simple App for Kids/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Very Simple App for Kids
+//
+//  Created by Quintin Smith on 11/27/14.
+//  Copyright (c) 2014 Quintin Smith. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
